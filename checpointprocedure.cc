080a018b08c76d2a2153d4b139651e0799ded5e2
Messaoud92 <77979134+Messaoud92@users.noreply.github.com>
2022-10-19 13:24:59 +0100

    Add files via upload

---
+++ checpointprocedure.cc
@@ -0,0 +1,44 @@
+ add_num(num:INTEGER) // num is the local variable for the procedure
+
+BEgIN
+  PROCEDURE produit_scalair(var t1,var t2) : reel
+              }
+     : integer                   { variable indice de boucle pour        }
+          ps : float                   { variable cumul resultat               }
+ 
+   for i in 0 to N-1 do
+     ps := ps + t1[i]*t2[i];
+    end
+
+END
+ALGORITHM orthogoneaux
+VAR
+     n,m,i:integer;
+   myData : INTEGER := 20;
+      var t1 : array [N] :float              
+           t2 : array [N] :float    
+
+BEGIN
+    do 
+	read(m);
+	read(n);
+    while(n>0 && m>0)
+		 for i in 0 to N-1 do
+   read(t1[i]);
+   read(t2[i];
+    end
+
+   produit_scalair(var t1,var t2); 
+END.
+
+  fonction produit scalair(t1,t2) : reel
+              }
+     : integer                   { variable indice de boucle pour        }
+          ps : float                   { variable cumul resultat               }
+   ps := 0.0;
+   for i in 0 to N-1 do
+     ps := ps + t1[i]*t2[i];
+    end
+    return  := ps;
+
+END
